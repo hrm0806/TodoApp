4542a49b5a88a02d9848f7bad28eddf8abf78239
hrm0806 <hiruma.s.aa@m.titech.ac.jp>
2024-10-07 18:25:21 +0900

    add delete

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -10,13 +10,11 @@ export async function GET() {
   return NextResponse.json(data.rows, { status: 200 });
 }
 
-//タスク追加
 export async function POST(req: NextRequest) {
   const db = new DBClient(); //DBClientクラスのインスタンスを生成する
   await db.client.connect(); //DB接続を開始する
 
   const { title, status, content } = await req.json();
-  //ここまでOK
   const query = `
       INSERT INTO tasks( title, status, content)
       VALUES ($1, $2, $3)
@@ -27,4 +25,24 @@ export async function POST(req: NextRequest) {
 
   await db.client.end(); //DB接続を終了する
   return NextResponse.json(data.rows[0], { status: 201 });
+}
+
+//タスク削除
+export async function DELETE(req: NextRequest) {
+  const db = new DBClient();
+  await db.client.connect();
+  const { id } = await req.json();
+
+  const query = `
+        DELETE FROM tasks WHERE id = $1
+        RETURNING *
+      `;
+  const values = [id];
+  await db.client.query(query, values);
+
+  await db.client.end();
+  return NextResponse.json(
+    { message: "Task deleted successfully" },
+    { status: 200 }
+  );
 }
--- src/components/atoms/button/DeleteButton.tsx
+++ src/components/atoms/button/DeleteButton.tsx
@@ -12,8 +12,17 @@ type ButtonProps = {
 const DeleteButton = ({ buttonname, id }: ButtonProps) => {
   const router = useRouter();
   const handleDelete = async () => {
-    await deleteTask(id);
-    router.refresh();
+    if (confirm("本当にこのタスクを削除しますか？")) {
+      const API_URL = process.env.NEXT_PUBLIC_API_URL;
+      await fetch(`${API_URL}/api/`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id }),
+      });
+      router.refresh();
+    }
   };
 
   return (
